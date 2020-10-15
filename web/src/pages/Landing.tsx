43986e6e322906fa1bae86f03ed0e84b5c7ddf48
Dan Buck <35202075+danbuck84@users.noreply.github.com>
2020-10-15 11:23:54 -0300

    day02

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -7,28 +7,28 @@ import '../styles/pages/landing.css';
 import logoImg from '../images/logo.svg';
 
 function Landing() {
-    return (
+  return (
 
-        <div id="page-landing">
-            <div className="content-wrapper">
-                <img src={logoImg} alt="Happy"/>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
 
-                <main>
-                    <h1>Leve felicidade para o mundo</h1>
-                    <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-                </main>
+        <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
 
-                <div className="location">
-                    <strong>São José dos Campos</strong>
-                    <span>São Paulo</span>
-                </div>
-
-                <Link to="/app" className="enter-app">
-                    <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-                </Link>
-            </div>
+        <div className="location">
+            <strong>São José dos Campos</strong>
+            <span>São Paulo</span>
         </div>
-    );
+
+        <Link to="/app" className="enter-app">
+            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -10,37 +10,37 @@ import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy"/>
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
 
-                    <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
 
-                <footer>
-                    <strong>São José dos Campos</strong>
-                    <span>São Paulo</span>
-                </footer>
-            </aside>
+        <footer>
+          <strong>São José dos Campos</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
 
-            <Map 
-                center={[-23.1973738,-45.8963594]}
-                zoom={15}
-                style={{ width: '100%', height: '100%' }}
-            >
-                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-                />
-            </Map>
+      <Map 
+        center={[-23.1973738,-45.8963594]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
 
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#FFF" />
-            </Link>
-        </div>
-    )
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  )
 }
 
 export default OrphanagesMap;
